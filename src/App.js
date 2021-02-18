74be02f82b5f92c0551881ed810a037944377ae7
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2021-02-18 11:56:59 +0530

    Add task done right

--- src/App.js
+++ src/App.js
@@ -1,42 +1,69 @@
 import "./styles.css";
-
-const AddTaskBar = () => {
-  return (
-    <div className="todo">
-      <div className="todo_addtask">
-        <div className="todo_addtask_text">ADD A TASK</div>
-        <div className="todo_addtask_input">
-          <input className="input" placeholder="Eg: Buy Groceries"></input>
-          <button className="addtask_button">+Add Task</button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const ToDoList = (props) => {
-  return (
-    <div className="todo_tasklist">
-      <ul className="list">
-        <li className="list_item">
-          <input className="checkStrike" type="checkbox"></input>
-          <span className="list_item_text">{props.value}</span>
-          <img
-            className="dustbin"
-            src="https://img.icons8.com/android/16/000000/delete.png"
-          />
-        </li>
-      </ul>
-    </div>
-  );
-};
+import { useState } from "react";
 
 export default function App() {
+  var newTask = "";
+
+  const [taskList, setTaskList] = useState([]);
+
+  const AddTaskBar = () => {
+    function newTaskUpdate(event) {
+      newTask = event.target.value;
+    }
+
+    function updateTaskList() {
+      setTaskList([...taskList, newTask]);
+    }
+
+    return (
+      <div className="todo">
+        <div className="todo_addtask">
+          <div className="todo_addtask_text">ADD A TASK</div>
+          <div className="todo_addtask_input">
+            <input
+              onChange={newTaskUpdate}
+              className="input"
+              placeholder="Eg: Buy Groceries"
+            ></input>
+            <button onClick={updateTaskList} className="addtask_button">
+              +Add Task
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const EachTaskDiv = (props) => {
+    if (!props.value) return null;
+    return (
+      <li className="list_item">
+        <input className="checkStrike" type="checkbox" />
+        <span className="list_item_text">{props.value}</span>
+        <img
+          className="dustbin"
+          src="https://img.icons8.com/android/16/000000/delete.png"
+        />
+      </li>
+    );
+  };
+
+  const Task = () => {
+    return (
+      <div class="todo_tasklist">
+        <ul class="list">
+          {taskList.map((item) => (
+            <EachTaskDiv value={item} />
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <AddTaskBar />
-      <ToDoList value="Read Books" />
-      <ToDoList value="Buy Groceries" />
+      <Task />
     </div>
   );
 }
